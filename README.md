b7c4da0f78bc5ec942b0ba14750aa5cdd44dfa62
codespren <username@users.noreply.github.com>
2024-08-09 10:32:15 +0300

    an adventure game

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# adventureGame
+# Adventure game
+An adventure game written in ><>.
+
+(More about ><>: https://esolangs.org/wiki/Fish)
+
+
+## Running the game
+1. Install python
+2. Install ><> interpreter: https://gist.github.com/anonymous/6392418
+3. Run the game: `python3 ~/fish/fish.py adventureGame.fish` (This assumes that the interpreter was installed to ~/fish)
---
+++ adventureGame.fish
@@ -0,0 +1,60 @@
+v   
+
+>>v                                                                                                                    v<
+  >>> a'    |        |       ___  ___  ___      |        'av                                                           ""
+    >  '   /|\      /|\      | |__| |__| |     /|\       'a v                                                           g
+     > '   /|\     //|\\     |    ___    |     /|\       'a  v                                                         yn
+      >' ___|____ __ |__ ____|vvvv| |vvvv|__ ___|___ __ _'a   v                                                        oiv< 
+    ^                                                      <  v                                                        ut""
+     ^                                                      < v                                                        rt !v<
+      ^                                                      <v       ;v?:o< 0a"2. You died. The end."a    <            eYr "
+va                                                            <        >   ^                                           aroe"  
+>0r>o:?vv                                                                                                              cgumbn
+   ^   <                                                                                                               te rua 
+v       <                                                                                                              irlatm 
+>"You are an adventurer travelling on a road in a magical forest."v                                                    o if  
+v                                                                 <                                                    nsv he
+>a"You see an abandoned castle nearby, visit the castle (y/n)?"0r>o:?vv                    >  v                        syeteh 
+                                                                 ^   <                        v                        .a n t
+v                                                                     <                     v1x0v                       dtew 
+> i'y'=?vv                                                                                    ^                        T hcay
+vaa     <                                                                                   > v <                     ;hreosa
+         !                                                                                    v                      v<eu n l
+ >a"Oooh! A damned hell hound has made the castle as its home! You must fight!"0r>o:?vv       >          ?v^          ? ornjs
+                                                                                 ^   <     ^      <                   :eyeiu 
+  v                                                                                   <           ^                   on s su
+  >                         a"Roll 1d2 (1 = victory, 2 = defeat). Press any key to roll."0r>o:?vi?^                  >^dftnto
+   v   &0<                                                                                 ^   <                      r.o a Y
+    v?i&1 v?:o< 0a"1. Victory! You slay the hound and what is that shiny thing on the floor... a gem!"a   <           0""""""
+          >   ^                                                                                                       ^<^<^<^    a<
+   >>v  >>a"You continue to follow the road. Soon you see an another traveller. A man with a mustache. Attack him (y/n)? "0r>o:?vv
+        ^                                      <                                                                            ^   <
+     >          a   a                         v                                                                       vv?='y'i   <
+v                                             v                                                                        >          ^ 
+!                                             !                                                                       >            v
+>^                                            >^                                             v"uoy fo htob dna uoy steerg nam ehT"a<
+          v                                 <           v                                 <  >                                     v
+         v                                 <           v                                 <  vv?:o<r0a".syaw nwo ruoy no eunitnoc " < 
+        v                                 <           v                                 <    >   ^
+       v                                 <           v                                 <    >a"Eventually you exit the forest and "v
+      v                                 <           v                                 <       vv?:o<r0a".revo era serutnevda ruoy" <
+     v                                 <           v                                 <         >   ^ 
+    v                                 <           v                                 <         >&?vv
+   v                                 <           v                                 <        v    <> "You didn't make any riches "  v
+  v                                 <           v                                 <               v"nevda ruoy fo seirots eht tub" <
+ v                                 <           v                                 <                >"tures will be heard for decade"v
+>          '  _   _              'a^          >          '     |||||||||       'a^                   ;v?:o<r0a".dne ehT .emoc ot s"<
+!>         ' //   \\             'a ^         !>         '    /         \      'a ^                   >   ^
+  >        '| |   | |            'a  ^          >        '   /  _    _   \     'a  ^        >"You sell the gem you found and live" v
+   >       ' \ ^^^  /            'a   ^          >       '  S   O    O    S    'a   ^       v" ruoy fo tser eht efil suorepsorp "  <
+    >      '  |o  o \            'a    ^          >      '   (    /      )     'a    ^      >"days. The end."0r>o:?vv
+     >     '    u    \________   'a     ^          >     '   (   (__     )     'a     ^                        ^   <
+      >    '    --            \  'a      ^          >    '   (  /// \\\  )     'a      ^           vaa              <
+       >   '    \     ____     \ 'a       ^          >   '   (    ___    )     'a       ^            v           <
+        >  '     || ||    || || \'a        ^          >  '    \         /      'a        ^          v           <
+         > '     || ||    || ||  'a         ^          > '     \_______/       'a         ^        >  '  \ /  'a^
+          >'     /  /     /  /   'av                    >'                     'av                  > ' - O - 'a ^
+ v                                 <            v                                <                   >'  / \  'a0r>o:?v;
+ >0r>o:?vv                                      >0r>o:?vv                                                         ^   <
+    ^   <                                          ^   <  
+^        <                                    ^         <
